a1e0eab8934695c395b24eab39a3cf37d3d33509
hogvarce <hogvarce@gmail.com>
2016-09-11 20:22:56 +0300

    start

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AboutPage extends Component {
+  render() {
+    return(
+      <div>
+        <h1>About page</h1>
+        <p>This application used React.</p>
+      </div>
+    )
+  }
+}
