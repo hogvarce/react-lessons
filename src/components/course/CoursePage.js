9c3e0aba4a623432bb37827f384f7ad8187f2aa1
hogvarce <hogvarce@gmail.com>
2016-09-13 15:53:31 +0300

    Добавлен async и mock

--- src/components/course/CoursePage.js
+++ src/components/course/CoursePage.js
@@ -2,57 +2,26 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseAction from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursePage extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {
-      course: {title: ''}
-    };
-
-    this.onClickSave = this.onClickSave.bind(this);
-    this.onTitleChange = this.onTitleChange.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
-    this.setState({ course: {title: ''} });
-  }
-
-  courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
   }
 
   render() {
+    const {courses} = this.props;
     return(
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
-        <h2>Add course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title}
-        />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave}
-        />
+        <CourseList courses={courses} />
       </div>
     );
   }
 }
 
 CoursePage.propTypes = {
-  courses: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired
+  courses: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -61,11 +30,5 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-function mapStateDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(courseAction, dispatch)
-  };
-}
 
-
-export default connect(mapStateToProps, mapStateDispatchToProps)(CoursePage);
+export default connect(mapStateToProps)(CoursePage);
